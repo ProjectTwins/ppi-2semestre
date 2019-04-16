d65182913c33f859074e103be75875937d37cd51
ProjectTwins <kloppos120@gmail.com>
2019-04-16 20:41:55 -0300

    Metodos Inserir,Atualizar e ProcurarRA, Criados

--- src/model/dao/impl/AlunoDaoJDBC.java
+++ src/model/dao/impl/AlunoDaoJDBC.java
@@ -3,7 +3,12 @@ package model.dao.impl;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import db.DB;
+import db.DbException;
 import entities.Aluno;
 import entities.Materias;
 import model.AlunoDAO;
@@ -20,13 +25,48 @@ public class AlunoDaoJDBC implements AlunoDAO{
 	
 	@Override
 	public void inserirAluno(Aluno obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO aluno" +
+					"(nome, ra, turma) " +
+					"VALUES"
+					+"(?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+					
+			st.setString(1, obj.getNome());
+			st.setInt(2, obj.getRa());
+			st.setString(3, obj.getTurma());
+			
+			st.executeUpdate();
+					 
+					
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void atualizarAluno(Aluno obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE aluno" +
+					"SET name = ?, turma = ?"+
+					"WHERE ra = ?");
+					
+			st.setString(1, obj.getNome());
+			st.setString(2, obj.getTurma());
+			st.setInt(3, obj.getRa());
+			
+			st.executeUpdate();
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -44,7 +84,18 @@ public class AlunoDaoJDBC implements AlunoDAO{
 
 	@Override
 	public void procurarPeloRa(Integer ra) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM tb_aluno AND tb_materia "+
+		"WHERE ra = ? ");
+			
+			st.setInt(1, ra);
+			st.executeUpdate();
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
