79d27d7a886824aba9e780d3aaa7a84d6c609ea6
ProjectTwins <kloppos120@gmail.com>
2019-04-22 20:29:49 -0300

    Classe testeVocacional finalizada

--- src/entities/testeVocacional.java
+++ src/entities/testeVocacional.java
@@ -2,5 +2,34 @@ package entities;
 
 public class testeVocacional {
 
-	private Integer escolha;
+	int pergunta1;
+	int pergunta2;
+	int pergunta3;
+	int pergunta4;
+	int pergunta5;
+
+	int pontosTotal = pergunta1 + pergunta2 + pergunta3 + pergunta4 + pergunta5;
+
+	public void setPergunta1(int ponto) {
+		this.pergunta1 = ponto;
+	}
+
+	public void setPergunta2(int ponto) {
+		this.pergunta2 = ponto;
+	}
+
+	public void setPergunta3(int ponto) {
+		this.pergunta3 = ponto;
+	}
+
+	public void setPergunta4(int ponto) {
+		this.pergunta4 = ponto;
+	}
+
+	public void setPergunta5(int ponto) {
+		this.pergunta5 = ponto;
+	}
+		
 }
+
+
